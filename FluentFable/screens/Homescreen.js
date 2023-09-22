d5b641aafc1208f46cef71155706ea0fae0f6f72
Paul Song <paulsong00@uni.minerva.edu>
2023-09-23 02:37:33 +0900

    got rid of close and save buttons. instead save and close by clicking on either half of screen

--- FluentFable/screens/Homescreen.js
+++ FluentFable/screens/Homescreen.js
@@ -1,17 +1,20 @@
-import { StyleSheet, Text, View, Modal, Button, TextInput } from 'react-native'
+import { StyleSheet, Text, View, Modal, Button, TextInput, TouchableOpacity } from 'react-native'
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react'
 // import callGPT from '../components/callGPT';
 import Translator from 'react-native-translator'
 
 const Homescreen = () => {
+  // required for GPT API call
   const axios = require('axios');
+  // state variables to keep track of modal, selected text, translated text, and main text
   const [modalVisible, setModalVisible] = useState(false);
   // const { data } = callGPT({query: 'List five cute animals.'})
-  const [value, setValue] = useState('');
-  const [result, setResult] = useState('');
+  const [selectedText, setSelectedText] = useState('');
+  const [translatedText, setTranslatedText] = useState('');
   const [text, setText] = useState('This is an example text that will be replaced \n by ChatGPT prompts in the future iterations.');
 
+  // small functions for opening and closing modal; might just get rid of these later
   const openModal = () => {
     setModalVisible(true);
   };
@@ -20,27 +23,25 @@ const Homescreen = () => {
     setModalVisible(false);
   };
 
-  // const handleLongPress = (text) => {
-  //   setValue(text);
-  //   openModal();
-  //   console.log({value})
-  // };
+  // ############ WORK NEEDED, SAVE WORD TO DATABASE ############
+  const saveWord = () => {
+    setModalVisible(false);
+    console.log('saving word...')
+  };
 
+  // when text is selected, output selected text value using text.substring method
   const handleTextSelectionChange = (selection) => {
     if (selection.start !== selection.end) {
        const selectedContent = text.substring(selection.start, selection.end)
-       console.log(selectedContent)
-       setValue(selectedContent);
-       openModal();
+       setSelectedText(selectedContent);
+       openModal(); 
     }
   } 
 
   return (
     <View style={styles.container}>
       <StatusBar style={styles.container} />
-      {/* <Text selectable={true} onSelect onPress={(text) => handleLongPress(text)}>
-          가위 바위 보라색
-      </Text> */}
+      {/* ############ MAKE FONT BIGGER AND SPACE THEM OUT MORE ############ */}
       <MyTextInput
         multiline={true}
         selectable={true}
@@ -54,23 +55,20 @@ const Homescreen = () => {
         animationType="slide"
         transparent={true}
       >
-        <View style={styles.modalOverlay}>
-          <View style={styles.modalContent}>
-            <View>
-              <Translator 
+        <View style={{flexDirection: 'row', flex: 1, justifyContent:'center'}}>
+          <TouchableOpacity style={styles.modalOverlayLeft} onPress = {() => saveWord()}></TouchableOpacity>
+          <TouchableOpacity style={styles.modalOverlayRight} onPress = {() => closeModal()}></TouchableOpacity>
+          <TouchableOpacity style={styles.modalContent} activeOpacity={1}>
+            <Translator 
                 from="en"
                 to="ko"
-                value={value}
-                onTranslated={(t) => setResult(t)}  
-              />
-              <Text>{result}</Text>
-            </View>
-            <View style={styles.buttonContainer}>
-              <Button title="Save" onPress={closeModal} />
-              <Button title="Close" onPress={closeModal} />
-            </View>
-          </View>
+                value={selectedText}
+                onTranslated={(t) => setTranslatedText(t)}  
+            />
+            <Text>{translatedText}</Text>
+          </TouchableOpacity>
         </View>
+        
       </Modal>
     </View>
   );
@@ -93,20 +91,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  modalOverlay: {
+  modalOverlayLeft: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background
+    backgroundColor: 'rgba(0, 10, 0, 0.5)', 
+  },
+  modalOverlayRight: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(50, 0, 0, 0.5)',
   },
   modalContent: {
+    position: 'absolute',
     backgroundColor: 'white',
     padding: 20,
     borderRadius: 10,
-  },
-  buttonContainer: {
-    flexDirection: 'row', // Horizontal layout
-    justifyContent: 'space-between', // Distribute space between buttons
-    marginTop: 20, // Add margin for spacing
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 350,
   },
 });
