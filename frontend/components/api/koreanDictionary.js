72759dd8639af86c64bcff5f55da9da6b7c8659e
Paul Song <paulsong00@uni.minerva.edu>
2024-04-25 00:53:01 +0900

    handle list of words

--- frontend/components/api/koreanDictionary.js
+++ frontend/components/api/koreanDictionary.js
@@ -6,43 +6,46 @@ import 'react-xml-parser';
 import {KOREAN_DICTIONARY_CLIENT_ID} from '@env'
 
 const koreanDictionary = ( {query} ) => {
-    const [dictionaryData, setData] = useState(null);
+    const [dictionaryData, setDictionaryData] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
     const client_id = KOREAN_DICTIONARY_CLIENT_ID;
     const api_url = `https://krdict.korean.go.kr/api/search`;
 
-    const options = {
-        method: 'GET',
-        url: api_url,
-        params: {
-            key: client_id,
-            q: query,
-            sort: 'popular',
-            translated: 'y',
-            trans_lang: '1',
-        }
-    };
-
-    const fetchData = async () => {    
+    //currenly only acccepts array of strings so it has to pass through the stemwordlist
+    const fetchData = async () => {
         setIsLoading(true);
         try {
-            const response = await axios.request(options)
-
-            if (!response.data) {
-                throw new Error("Empty response data");
-            }
-            
-            // console.log(response.data)  <-- more info about response
-            var XMLParser = require('react-xml-parser');
-            var xml = new XMLParser().parseFromString(response.data);
-            const translations = xml.getElementsByTagName('trans_word').slice(0,3).map(dict => dict.value.slice(0,-2));
-            console.log('translations..', translations);
-
-            setData(translations);
-        } catch(error) {
-            console.log("ERRORORERERR")
+            console.log('attempting..');
+            const results = await Promise.all(
+                query.map(async (q) => {
+                    console.log('q', q);
+                    const options = {
+                        method: 'GET',
+                        url: api_url,
+                        params: {
+                            key: client_id,
+                            q: q,
+                            sort: 'popular',
+                            translated: 'y',
+                            trans_lang: '1',
+                        }
+                    };
+                    const response = await axios.request(options);
+                    if (!response.data) {
+                        throw new Error("Empty response data");
+                    }
+                    var XMLParser = require('react-xml-parser');
+                    var xml = new XMLParser().parseFromString(response.data);
+                    const translations = xml.getElementsByTagName('trans_word').slice(0, 3).map(dict => dict.value.slice(0, -2));
+                    console.log('translations', translations);
+                    return translations;
+                })
+            );
+            setDictionaryData(results);
+        } catch (error) {
+            console.log('Error finding definitions:', error.message);
             setError(error.message);
         } finally {
             setIsLoading(false);
