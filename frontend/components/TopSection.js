f4acad078dad3f93087dab117178dcc480a171ae
Paul Song <paulsong00@uni.minerva.edu>
2024-04-27 03:16:42 +0900

    completely upgraded saving feature

--- frontend/components/TopSection.js
+++ frontend/components/TopSection.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, TouchableOpacity, ScrollView, Switch } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, ScrollView, Switch, CheckBox } from 'react-native';
 
 import Translator from 'react-native-translator';
 import koreanDictionary from './api/koreanDictionary';
 import stemWord from './api/stemWord';
 
-import { Ionicons } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
-import { insertData, removeData, wordExists } from './Database';
+import { insertData, removeData } from './Database';
 
 const TopSection = ({ highlightedWord }) => {
+    // handles 'more' and 'less' to control how much info is displayed from dictionary def
     const [expandedWords, setExpandedWords] = useState([]);
     const toggleExpanded = (word) => {
         setExpandedWords((prevExpandedWords) =>
@@ -22,106 +22,106 @@ const TopSection = ({ highlightedWord }) => {
 
     const [translated, setTranslated] = useState(''); 
     const [type, setType] = useState('papago');
-       
-    const [isContent1Visible, setIsContent1Visible] = useState(true);
-    const [isSaved, setIsSaved] = useState(false); 
+    
+    const [transNotDict, setTransNotDict] = useState(true);
+    const [savedWords, setSavedWords] = useState({});
 
     const stemWordList  = stemWord({ query: highlightedWord });
     const { dictionaryData } = koreanDictionary({ query: stemWordList });
     
     const toggleContent = () => {
-        setIsContent1Visible(!isContent1Visible);
+        setTransNotDict(!transNotDict);
     };
-
-    // this use effect and promise chaining handles asynchronous nature of data operations
-    useEffect(() => {
-        wordExists(highlightedWord)
-            .then(exists => {
-                console.log(exists)
-                setIsSaved(exists);
-            })
-            .catch(error => {
-                console.error('Error checking if word exists:', error);
-            });
-    }, [highlightedWord]);
 
     const handleTypeChange = () => {
         setType(type === 'papago' ? 'google' : 'papago');
     };
 
     // add word to database and toggle save
-    const toggleSave = async () => {
-        await insertData(highlightedWord,"umm",dictionaryData.join(", "),"unorganized");
-        setIsSaved(true);
+    const toggleSave = async (word, origin, definition) => {
+        const updatedSavedWords = { ...savedWords, [(word, origin, definition)]: true };
+        setSavedWords(updatedSavedWords);
+        insertData(word, origin, definition, "unorganized");
     };
 
     // remove word from database and toggle unsave
-    const toggleUnSave = async () => {
-        await removeData(highlightedWord);
-        setIsSaved(false);
+    const toggleUnSave = async (word, origin, definition) => {
+        const updatedSavedWords = { ...savedWords };
+        delete updatedSavedWords[(word, origin, definition)];
+        setSavedWords(updatedSavedWords);
+        removeData(word);
     }
+
+    const isWordSaved = (word, origin, definition) => savedWords[(word, origin, definition)] === true;
 
     return (
         <View style={styles.topSection}>
 
-            <Text style={{ fontSize: 18, position: 'absolute', top:8, left:13 }}> 
-                {highlightedWord}
-            </Text>
+            <Text style={{ fontSize: 18, position: 'absolute', top:8, left:13 }}>{highlightedWord}</Text>
             
-            {isContent1Visible ? (
-                <TouchableOpacity onPress={toggleContent} style={styles.lookup}>
-                    <MaterialIcons name="translate" size={28} color="black" />
-                </TouchableOpacity>
-            ) : (
-                <TouchableOpacity onPress={toggleContent} style={styles.lookup}>
-                    <Feather name="book-open" size={28} color="black" />
-                </TouchableOpacity>
-            )}
-
-            {isSaved ? (
-                <TouchableOpacity onPress={toggleUnSave} style={styles.save}>
-                    <MaterialCommunityIcons name="content-save-check" size={28} color="black" />
-                </TouchableOpacity>
-            ) : (
-                <TouchableOpacity onPress={toggleSave} style={styles.save}>
-                    <Ionicons name="save-outline" size={28} color="black" />
-                </TouchableOpacity>
-            )}
-    
-            {isContent1Visible ? (
+            {/* toggle between translator and dictionary */}
+            <TouchableOpacity onPress={toggleContent} style={styles.lookup}>
+                {transNotDict ? <MaterialIcons name="translate" size={25} color="black" /> : <Feather name="book-open" size={25} color="black" />}
+            </TouchableOpacity>
+                
+            {transNotDict ? (
             <ScrollView style={{ marginTop: 30, marginLeft: 0 }}>
                 {stemWordList.map((word, index) => (
                     <View key={index}>
                         {dictionaryData[index] && dictionaryData[index].length > 0 ? (
                             <>
-                            <View style={{flexDirection: 'row'}}>
-                                <Text style={{ fontWeight: 'bold' }}>{word}</Text>
-                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                    <Text style={{ marginHorizontal: 5 }}>(</Text>
-                                    <TouchableOpacity>
-                                        <Text>{dictionaryData[index][0].origin}</Text>
-                                    </TouchableOpacity>
-                                    <Text style={{ marginHorizontal: 5 }}>)</Text>
-                                    <Text>{dictionaryData[index][0].transWord}</Text>
+                            {/* this shows the first entry of the dictionary definition */}
+                            <View>
+
+                                <TouchableOpacity onPress={() => isWordSaved(word, dictionaryData[index][0].origin, dictionaryData[index][0].transWord) 
+                                    ? toggleUnSave(word, dictionaryData[index][0].origin, dictionaryData[index][0].transWord) 
+                                    : toggleSave(word, dictionaryData[index][0].origin, dictionaryData[index][0].transWord)} 
+                                    style={styles.save}>
+                                    <AntDesign name={isWordSaved(word, dictionaryData[index][0].origin, dictionaryData[index][0].transWord) 
+                                        ? "checksquare" : "checksquareo"} size={15} color="black" />
+                                </TouchableOpacity>
+
+                                <View style={styles.content}>
+                                    <Text style={{ fontWeight: 'bold' }}>{word}</Text>
+                                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                        <Text style={{ marginHorizontal: 5 }}>(</Text>
+                                        <TouchableOpacity>
+                                            <Text>{dictionaryData[index][0].origin}</Text>
+                                        </TouchableOpacity>
+                                        <Text style={{ marginHorizontal: 5 }}>)</Text>
+                                        <Text>{dictionaryData[index][0].transWord}</Text>
+                                    </View>
+                                    {dictionaryData[index].length > 1 ? (
+                                        <TouchableOpacity onPress={() => toggleExpanded(word)}>
+                                            <Text style={{ color: 'blue', textDecorationLine: 'underline', marginLeft: 5}}>
+                                                {expandedWords.includes(word) ? 'less' : 'more'}
+                                            </Text>
+                                        </TouchableOpacity>
+                                    ) : null}
                                 </View>
-                                {dictionaryData[index].length > 1 ? (
-                                    <TouchableOpacity onPress={() => toggleExpanded(word)}>
-                                        <Text style={{ color: 'blue', textDecorationLine: 'underline', marginLeft: 5}}>
-                                            {expandedWords.includes(word) ? 'less' : 'more'}
-                                        </Text>
-                                    </TouchableOpacity>
-                                ) : null}
+
                             </View>
+                            {/* shows the rest of the definitions once 'more' is pressed */}
                             {expandedWords.includes(word) &&
                                 dictionaryData[index].slice(1).map((entry, i) => (
                                     <View key={i} style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                        <Text>{entry.word}</Text>
-                                        <Text style={{ marginHorizontal: 5 }}>(</Text>
-                                        <TouchableOpacity>
-                                            <Text>{entry.origin}</Text>
+
+                                        <TouchableOpacity onPress={() => isWordSaved(entry.word, entry.origin, entry.transWord) 
+                                            ? toggleUnSave(entry.word, entry.origin, entry.transWord) 
+                                            : toggleSave(entry.word, entry.origin, entry.transWord)} style={styles.save}>
+                                            <AntDesign name={isWordSaved(entry.word, entry.origin, entry.transWord) 
+                                                ? "checksquare" : "checksquareo"} size={15} color="black" style={{ opacity: 0.5 }}/>
                                         </TouchableOpacity>
-                                        <Text style={{ marginHorizontal: 5 }}>)</Text>
-                                        <Text>{entry.transWord}</Text>
+
+                                        <View style={styles.content}>
+                                            <Text>{entry.word}</Text>
+                                            <Text style={{ marginHorizontal: 5 }}>(</Text>
+                                            <TouchableOpacity>
+                                                <Text>{entry.origin}</Text>
+                                            </TouchableOpacity>
+                                            <Text style={{ marginHorizontal: 5 }}>)</Text>
+                                            <Text>{entry.transWord}</Text>
+                                        </View>
                                     </View>
                                 ))}
                             </>
@@ -132,10 +132,11 @@ const TopSection = ({ highlightedWord }) => {
                 ))}
             </ScrollView>
             ) : (
-            <ScrollView style={{ marginTop: 30, marginLeft: 0 }}>
+            <ScrollView style={{ marginTop: 30 }}>
                 <Switch
-                value={type === 'google'}
-                onValueChange={handleTypeChange}
+                    value={type === 'google'}
+                    onValueChange={handleTypeChange}
+                    style={{ top: -12, right: -42, transform: [{ scaleX: .8 }, { scaleY: .8 }] }}
                 />
                 <Translator
                 from="ko"
@@ -145,7 +146,9 @@ const TopSection = ({ highlightedWord }) => {
                 onTranslated={(t) => setTranslated(t)}
                 />
                 <Text style={{ position: 'absolute', top: 0}}>
-                    {type}: {translated}
+                    {type === 'papago' 
+                    ? <Text><Text style={{ fontWeight: 'bold' }}>Papago</Text>: {translated}</Text> 
+                    : <Text><Text style={{ fontWeight: 'bold' }}>Google</Text>: {translated}</Text>}
                 </Text>
             </ScrollView>
             )}
@@ -169,24 +172,24 @@ const styles = StyleSheet.create({
         width: '100%',
     },
     save: {
-        width: 30, // Set width to make it square
-        height: 30, // Set height to make it square
         position: 'absolute', // Set position to absolute
         top: 5, // Adjust top position as needed
-        right: 5, // Adjust right position as needed
+        left: 5, // Adjust right position as needed
         borderRadius: 5, // Example border radius
         justifyContent: 'center', // Center content vertically
         alignItems: 'center', // Center content horizontally
     },
     lookup: {
-        width: 30, // Set width to make it square
-        height: 30, // Set height to make it square
         position: 'absolute', // Set position to absolute
-        top: 5, // Adjust top position as needed
-        right: 45, // Adjust right position as needed
+        top: 10, // Adjust top position as needed
+        right: 13, // Adjust right position as needed
         borderRadius: 5, // Example border radius
         justifyContent: 'center', // Center content vertically
         alignItems: 'center', // Center content horizontally
+    },
+    content: {
+        left: 25,
+        flexDirection: 'row'
     }
 
 });
