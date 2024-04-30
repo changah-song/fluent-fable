3b8a3de3343a375bb85b5e7263ff24140a4c69ab
Paul Song <paulsong00@uni.minerva.edu>
2024-05-01 01:59:18 +0900

    testing translator component

--- frontend/screens/Flashcard.js
+++ frontend/screens/Flashcard.js
@@ -4,9 +4,11 @@ import { useFocusEffect } from '@react-navigation/native'; // Import useFocusEff
 
 import { viewData } from '../components/Database';
 
+import Translator from 'react-native-translator';
+
 const Flashcard = () => {
   const [words, setWords] = useState([]);
-
+  console.log()
   const fetchWords = () => {
     viewData()
       .then(data => {
@@ -25,11 +27,30 @@ const Flashcard = () => {
       fetchWords(); // Fetch data whenever screen is focused
     }, [])
   );
-   
+
+  const [result, setResult] = useState('');
+  const [result2, setResult2] = useState('');
+
   const showWords = () => {
     return words.map((word, index) => {
       return (
         <View key={index}>
+          <Translator
+            from="ko"
+            to="en"
+            value={"아이돌 팬들은 최애 멤버의 포토카드를 활용하기도 한다"}
+            type={"google"}
+            onTranslated={(t) => setResult(t)}
+          />
+          <Text>GOOGLE: {result}</Text>
+          <Translator
+            from="ko"
+            to="en"
+            value={"아이돌 팬들은 최애 멤버의 포토카드를 활용하기도 한다."}
+            type={"papago"}
+            onTranslated={(t) => setResult2(t)}
+          />
+          <Text>PAPAGO: {result2}</Text>
           {word.word && <Text>Word: {word.word}</Text>}
           {word.def && <Text>Definition: {word.def}</Text>}
           {word.hanja && <Text>Hanja: {word.hanja}</Text>}
