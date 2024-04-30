ce472f14206aefa2aad1b40bc71b498ea7dc81e4
Paul Song <paulsong00@uni.minerva.edu>
2024-05-01 01:59:08 +0900

    better organization, function outside return

--- frontend/components/TopSection.js
+++ frontend/components/TopSection.js
@@ -61,6 +61,95 @@ const TopSection = ({ highlightedWord }) => {
 
     const isWordSaved = (word, origin, definition) => savedWords[(word, origin, definition)] === true;
 
+    const renderDictionaryContent = () => (
+        <ScrollView style={{ marginTop: 30, marginLeft: 0 }}>
+            {stemWordList.map((word, index) => (
+                <View key={index}>
+                    {dictionaryData[index] && dictionaryData[index].length > 0 ? (
+                        <>
+                        {/* this shows the first entry of the dictionary definition */}
+                        <View>
+
+                            <TouchableOpacity onPress={() => isWordSaved(word, dictionaryData[index][0].origin, dictionaryData[index][0].transWord) 
+                                ? toggleUnSave(word, dictionaryData[index][0].origin, dictionaryData[index][0].transWord) 
+                                : toggleSave(word, dictionaryData[index][0].origin, dictionaryData[index][0].transWord)} 
+                                style={styles.save}>
+                                <AntDesign name={isWordSaved(word, dictionaryData[index][0].origin, dictionaryData[index][0].transWord) 
+                                    ? "checksquare" : "checksquareo"} size={15} color="black" />
+                            </TouchableOpacity>
+
+                            <View style={styles.content}>
+                                <Text style={{ fontWeight: 'bold' }}>{word}</Text>
+                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                    <Text style={{ marginHorizontal: 5 }}>(</Text>
+                                    <TouchableOpacity>
+                                        <Text>{dictionaryData[index][0].origin}</Text>
+                                    </TouchableOpacity>
+                                    <Text style={{ marginHorizontal: 5 }}>)</Text>
+                                    <Text>{dictionaryData[index][0].transWord}</Text>
+                                </View>
+                                {dictionaryData[index].length > 1 ? (
+                                    <TouchableOpacity onPress={() => toggleExpanded(word)}>
+                                        <Text style={{ color: 'blue', textDecorationLine: 'underline', marginLeft: 5}}>
+                                            {expandedWords.includes(word) ? 'less' : 'more'}
+                                        </Text>
+                                    </TouchableOpacity>
+                                ) : null}
+                            </View>
+
+                        </View>
+                        {/* shows the rest of the definitions once 'more' is pressed */}
+                        {expandedWords.includes(word) &&
+                            dictionaryData[index].slice(1).map((entry, i) => (
+                                <View key={i} style={{ flexDirection: 'row', alignItems: 'center' }}>
+
+                                    <TouchableOpacity onPress={() => isWordSaved(entry.word, entry.origin, entry.transWord) 
+                                        ? toggleUnSave(entry.word, entry.origin, entry.transWord) 
+                                        : toggleSave(entry.word, entry.origin, entry.transWord)} style={styles.save}>
+                                        <AntDesign name={isWordSaved(entry.word, entry.origin, entry.transWord) 
+                                            ? "checksquare" : "checksquareo"} size={15} color="black" style={{ opacity: 0.5 }}/>
+                                    </TouchableOpacity>
+
+                                    <View style={styles.content}>
+                                        <Text>{entry.word}</Text>
+                                        <Text style={{ marginHorizontal: 5 }}>(</Text>
+                                        <TouchableOpacity>
+                                            <Text>{entry.origin}</Text>
+                                        </TouchableOpacity>
+                                            <Text style={{ marginHorizontal: 5 }}>)</Text>
+                                            <Text>{entry.transWord}</Text>
+                                        </View>
+                                    </View>
+                                ))}
+                            </>
+                        ) : (
+                            <Text> "Loading..." </Text>
+                        )}
+                    </View>
+                ))}
+        </ScrollView>
+    );
+
+    const renderTranslationContent = () => (
+        <ScrollView style={{ marginTop: 30 }}>
+            <Switch
+                value={type === 'papago'}
+                onValueChange={handleTypeChange}
+                style={{ top: -12, right: -42, transform: [{ scaleX: .8 }, { scaleY: .8 }] }}
+            />
+            <Translator
+            from="ko"
+            to="en"
+            value={highlightedWord}
+            type={type}
+            onTranslated={(t) => setTranslated(t)}
+            />
+            <Text style={{ position: 'absolute', top: 0, width: '94%'}}>
+                <Text>{translated}</Text>
+            </Text>
+        </ScrollView>   
+    );
+
     return (
         <View style={styles.topSection}>
 
@@ -79,96 +168,9 @@ const TopSection = ({ highlightedWord }) => {
             <TouchableOpacity onPress={toggleContent} style={styles.lookup}>
                 {dictMode ? <MaterialIcons name="translate" size={25} color="black" /> : <Feather name="book-open" size={25} color="black" />}
             </TouchableOpacity>
-                
-            {dictMode ? (
-            <ScrollView style={{ marginTop: 30, marginLeft: 0 }}>
-                {stemWordList.map((word, index) => (
-                    <View key={index}>
-                        {dictionaryData[index] && dictionaryData[index].length > 0 ? (
-                            <>
-                            {/* this shows the first entry of the dictionary definition */}
-                            <View>
 
-                                <TouchableOpacity onPress={() => isWordSaved(word, dictionaryData[index][0].origin, dictionaryData[index][0].transWord) 
-                                    ? toggleUnSave(word, dictionaryData[index][0].origin, dictionaryData[index][0].transWord) 
-                                    : toggleSave(word, dictionaryData[index][0].origin, dictionaryData[index][0].transWord)} 
-                                    style={styles.save}>
-                                    <AntDesign name={isWordSaved(word, dictionaryData[index][0].origin, dictionaryData[index][0].transWord) 
-                                        ? "checksquare" : "checksquareo"} size={15} color="black" />
-                                </TouchableOpacity>
-
-                                <View style={styles.content}>
-                                    <Text style={{ fontWeight: 'bold' }}>{word}</Text>
-                                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                        <Text style={{ marginHorizontal: 5 }}>(</Text>
-                                        <TouchableOpacity>
-                                            <Text>{dictionaryData[index][0].origin}</Text>
-                                        </TouchableOpacity>
-                                        <Text style={{ marginHorizontal: 5 }}>)</Text>
-                                        <Text>{dictionaryData[index][0].transWord}</Text>
-                                    </View>
-                                    {dictionaryData[index].length > 1 ? (
-                                        <TouchableOpacity onPress={() => toggleExpanded(word)}>
-                                            <Text style={{ color: 'blue', textDecorationLine: 'underline', marginLeft: 5}}>
-                                                {expandedWords.includes(word) ? 'less' : 'more'}
-                                            </Text>
-                                        </TouchableOpacity>
-                                    ) : null}
-                                </View>
-
-                            </View>
-                            {/* shows the rest of the definitions once 'more' is pressed */}
-                            {expandedWords.includes(word) &&
-                                dictionaryData[index].slice(1).map((entry, i) => (
-                                    <View key={i} style={{ flexDirection: 'row', alignItems: 'center' }}>
-
-                                        <TouchableOpacity onPress={() => isWordSaved(entry.word, entry.origin, entry.transWord) 
-                                            ? toggleUnSave(entry.word, entry.origin, entry.transWord) 
-                                            : toggleSave(entry.word, entry.origin, entry.transWord)} style={styles.save}>
-                                            <AntDesign name={isWordSaved(entry.word, entry.origin, entry.transWord) 
-                                                ? "checksquare" : "checksquareo"} size={15} color="black" style={{ opacity: 0.5 }}/>
-                                        </TouchableOpacity>
-
-                                        <View style={styles.content}>
-                                            <Text>{entry.word}</Text>
-                                            <Text style={{ marginHorizontal: 5 }}>(</Text>
-                                            <TouchableOpacity>
-                                                <Text>{entry.origin}</Text>
-                                            </TouchableOpacity>
-                                            <Text style={{ marginHorizontal: 5 }}>)</Text>
-                                            <Text>{entry.transWord}</Text>
-                                        </View>
-                                    </View>
-                                ))}
-                            </>
-                        ) : (
-                            <Text> "Loading..." </Text>
-                        )}
-                    </View>
-                ))}
-            </ScrollView>
-            ) : (
-            <ScrollView style={{ marginTop: 30 }}>
-                <Switch
-                    value={type === 'papago'}
-                    onValueChange={handleTypeChange}
-                    style={{ top: -12, right: -42, transform: [{ scaleX: .8 }, { scaleY: .8 }] }}
-                />
-                <Translator
-                from="ko"
-                to="en"
-                value={highlightedWord}
-                type={type}
-                onTranslated={(t) => setTranslated(t)}
-                />
-                <Text style={{ position: 'absolute', top: 0, width: '94%'}}>
-                    {type === 'papago' 
-                    ? <Text>{translated}</Text> 
-                    : <Text>{translated}</Text>}
-                </Text>
-            </ScrollView>
-            )}
-            
+            {/* show either dictionary or translator content depending on dictMdoe status */}
+            {dictMode ? renderDictionaryContent() : renderTranslationContent()}
         </View>
     );
   };
