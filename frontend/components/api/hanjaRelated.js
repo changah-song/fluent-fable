61dd6b971642bdd654a2fbb5dfc8b6a87ae79378
Paul Song <paulsong00@uni.minerva.edu>
2024-05-06 03:01:07 +0900

    api call now returns specific data we want

--- frontend/components/api/hanjaRelated.js
+++ frontend/components/api/hanjaRelated.js
@@ -1,37 +1,55 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import 'react-xml-parser';
 
-const hanjaRelated = ( {hanja} ) => {
-    const [content, setContent] = useState("");
+const hanjaRelated = ({ query }) => {
+    // list of array [korean, definition, hanja]
+    const [content, setContent] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
-    
-    const url = `https://koreanhanja.app/`;
 
-    const options = {
-        method: 'get',
-        url: url,
-        params: {
-            text: query
-        }
-    };
+    const url = `https://koreanhanja.app/`;
 
     const fetchData = async () => {
         try {
-            const response = await axios.request(options);
-            if (!response.data) {
-                throw new Error("Empty response data");
-            }
+            let result = [];
+            const response = await axios.get(url, {text: query});
             console.log(response);
-            setContent(response);
+            // Extract HTML content from the response
+            const htmlContent = response.data;
+
+            // Parse HTML content using DOMParser
+            var ReactXmlParser = require('react-xml-parser');
+            const htmlDocument = new ReactXmlParser().parseFromString(htmlContent);
+            
+            // Find all tables in the HTML document
+            const rows = htmlDocument.getElementsByTagName('tr');
+
+            rows.forEach(row => {
+                const temp = [];
+                // access the hanja
+                const hanjaPart = row.getElementsByTagName('td')[0].getElementsByTagName('a')[0].value;
+                // access the korean and definition
+                const korDefPart = row.getElementsByTagName('td');
+                korDefPart.forEach(cell => {
+                    // Access the content of the cell
+                    temp.push(cell.value.trim())
+                });
+                temp.push(hanjaPart)
+                result.push(temp.slice(1))
+            });
+            
+            setContent(result);
+            console.log('hmmm', query, result);
+
         } catch(error) {
-            console.error("Error fetching hanja data:", error)
+            console.error("Error fetching data:", error)
         }
     }
 
     useEffect(() => {
         fetchData();
-    }, [hanja]);
+    }, [query]);
 
     return content;
 }
