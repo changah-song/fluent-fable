19b028552455e42557dcad9c8c16c082db0c80e2
Paul Song <paulsong00@uni.minerva.edu>
2024-05-08 16:51:34 +0900

    parent file for the learning screen

---
+++ frontend/screens/Learn.js
@@ -0,0 +1,36 @@
+// Variables needed: LEARNED, day, 
+
+// TrackProgress.js
+    // use LEARNED variable
+
+// ProgressBar.js
+    // need database
+        // count of every 'level' attribute of data elements
+
+// Flashcard.js
+    // need database
+    // HanjaDetails.js
+    // update LEARNED variable
+
+    // logic: if swiped right, move onto next color, swipe left stays same color
+    //        show green after 1 week
+    //        show yellow after 3 days
+    //        show red after 1 day
+
+import { Text, View } from 'react-native'
+import React , { useState, useEffect } from 'react'
+import ActivityChecker from '../components/Learn/ActivityChecker';
+
+const Learn = () => {
+
+    return (
+        <View>
+            <ActivityChecker/>
+            <Text>Flashchard section</Text>
+            
+        </View>
+    )
+
+};  
+
+export default Learn
