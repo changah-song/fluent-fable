f44301e55e08c231f3256624d09ab074376c3736
Paul Song <paulsong00@uni.minerva.edu>
2024-05-22 12:39:45 +0900

    better vocab overview update

--- frontend/components/Learn/Overview.js
+++ frontend/components/Learn/Overview.js
@@ -3,17 +3,19 @@ import { Text, View, StyleSheet } from 'react-native';
 const Overview = () => {
     return (
         <View style={styles.container}>
-            <View style={styles.section}>
+            <View style={{alignItems: 'center', borderWidth: 1, borderRadius: 2, margin: 2, marginBottom: 5}}>
                 <Text>unorganized</Text>
             </View>
-            <View style={[styles.section]}>
-                <Text>bad</Text>
+            <View style={styles.slots}>
+                <View style={styles.section}>
+                    <Text>good</Text>
+                </View>
+                <View style={styles.section}>
+                    <Text>mid</Text>
+                </View>
+                <View style={styles.section}>
+                    <Text>bad</Text>
             </View>
-            <View style={styles.section}>
-                <Text>mid</Text>
-            </View>
-            <View style={styles.section}>
-                <Text>good</Text>
             </View>
         </View>
     )
@@ -21,11 +23,20 @@ const Overview = () => {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'red',
+        backgroundColor: 'pink',
+        width: '95%',
+        alignSelf: 'center'
     },
+    slots: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    }, 
     section: {
-        backgroundColor: 'blue',
-        width: '50%',
+        width: '32%',
+        borderWidth: 1,
+        borderRadius: 5,
+        margin: 2,
+        alignItems: 'center'
     }
 })
 
